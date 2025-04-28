346331c3477f2077a1aade123e988228f5882f4b
shatong <843391622@qq.com>
2025-04-28 14:44:30 +0800

    feat: 添加Vercel Analytics集成以跟踪应用使用情况
    在package.json中新增了@vercel/analytics依赖，并在main.ts中初始化了Analytics服务，以便跟踪应用的使用情况并收集相关数据。

--- examples/main.ts
+++ examples/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 // @ts-expect-error vue
 import App from './App.vue';
+import { inject } from '@vercel/analytics';
 
 const app = createApp(App);
 app.mount('#app');
+inject();
