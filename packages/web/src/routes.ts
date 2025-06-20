b77a60167666719f4e12851379ee62283f41033c
shatong <843391622@qq.com>
2025-06-20 16:24:14 +0800

    fix(routes): 更新PianoRoll组件的导入和路由名称
    - 将pianoRoll.vue组件的导入名称更改为PianoRoll，以符合组件命名规范
    - 更新路由配置中的名称，从'pianoRoll'更改为'PianoRoll'，提升代码一致性

--- packages/web/src/routes.ts
+++ packages/web/src/routes.ts
@@ -1,8 +1,8 @@
 import { type RouteRecordRaw } from 'vue-router';
 import Home from './pages/Home.vue';
-import pianoRoll from './pages/pianoRoll.vue';
+import PianoRoll from './pages/PianoRoll.vue';
 
 export const routes: RouteRecordRaw[] = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/piano-roll', name: 'pianoRoll', component: pianoRoll },
+  { path: '/piano-roll', name: 'PianoRoll', component: PianoRoll },
 ];
